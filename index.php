8bc3a00b2643f6b4c8f61e25d1f137d979bdf8e3
Thabani Chris Njoko <44303989+thabanichris@users.noreply.github.com>
2023-01-31 09:30:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-        <title> </title>
+        <title> Ozow Payment Gateway |  POST </title>
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
         <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     </head>
-    
     <body>
-        <?php
+    <?php
         if(isset($_GET['transactionid']) && isset($_GET['orderid']) && isset($_GET['amount'])){
             // Set Variables
             $transId= $_GET['transactionid'];
@@ -18,31 +17,28 @@
             $amount = $_GET['amount'];
             
             // Concatenate all the post variables
-            $a = "TSTSTE0001"."ZA"."ZAR".$amount.$orderId.$transId."http://demo.ozow.com/cancel.aspx"."http://demo.ozow.com/success.aspx"."http://demo.ozow.com/notify.aspx"."false"."215114531aff7134a94c88ceea48e";
+            $values = "TSTSTE0001"."ZA"."ZAR".$amount.$orderId.$transId."http://demo.ozow.com/cancel.aspx"."http://demo.ozow.com/success.aspx"."http://demo.ozow.com/notify.aspx"."false"."215114531aff7134a94c88ceea48e";
             // Convert the above concatenated string to lowercase
-            $a = strtolower($a);
+            $values = strtolower($values);
             // Generate a SHA512 hash of the lowercase concatenated string
-            $hash = hash('SHA512',$a);
-        ?>
-        <form action="https://pay.ozow.com" method="POST">
-           <input type="hidden" name="SiteCode" value="TSTSTE0001">
-           <input type="hidden" name="CountryCode" value="ZA">
-           <input type="hidden" name="CurrencyCode" value="ZAR">
-           
-           <input type="hidden" name="Amount" value="<?=$amount;?>">
-           <input type="hidden" name="TransactionReference" value="<?=$orderId;?>">
-           <input type="hidden" name="BankReference" value="<?=$transId;?>">
-           
-           <input type="hidden" name="CancelUrl" value="http://demo.ozow.com/cancel.aspx">
-           <input type="hidden" name="SuccessUrl" value="http://demo.ozow.com/success.aspx">
-           <input type="hidden" name="NotifyUrl" value="http://demo.ozow.com/notify.aspx">
-           
-           <input type="hidden" class="form-control" name="HashCheck" value="<?=$hash;?>">
-           
-           <input type="hidden" name="IsTest" value="false">
-           <input type="submit" class="btn btn-success" value="Click to Checkout Using Ozow">
-        </form>
-        <?php } ?>
-
+            $hash = hash('SHA512',$values);
+            
+        echo '
+            <form action="https://pay.ozow.com" method="POST">
+               <input type="hidden" name="SiteCode" value="TSTSTE0001">
+               <input type="hidden" name="CountryCode" value="ZA">
+               <input type="hidden" name="CurrencyCode" value="ZAR">
+               <input type="hidden" name="Amount" value="'.$amount.'">
+               <input type="hidden" name="TransactionReference" value="'.$orderId.'">
+               <input type="hidden" name="BankReference" value="'.$transId.'">
+               <input type="hidden" name="CancelUrl" value="http://demo.ozow.com/cancel.aspx"> // Replace with your Cancel Url
+               <input type="hidden" name="SuccessUrl" value="http://demo.ozow.com/success.aspx"> // Replace with your Success Url
+               <input type="hidden" name="NotifyUrl" value="http://demo.ozow.com/notify.aspx"> // Replace with your Notify URL
+               <input type="hidden" class="form-control" name="HashCheck" value="'.$hash.'">
+               <input type="hidden" name="IsTest" value="false">
+               <input type="submit" class="btn btn-success" value="Click to Checkout Using Ozow">
+            </form>';
+        } //endif
+    ?>
     </body>
 </html>
